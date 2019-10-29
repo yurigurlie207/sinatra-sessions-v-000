05893589c5218a07987cc85ba5613a2e678ae901
Adrienne Yao <adrienne.yao@icloud.com>
2019-10-29 15:37:47 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -21,6 +21,7 @@ class App < Sinatra::Base
 
   get '/set' do
     # set the :foo key of the session hash equal to 'hello' here!
+    
     if session[:foo] == 'hello'
       redirect '/fetch'
     else
